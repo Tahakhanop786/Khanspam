71a23bb32e4a29deea2574929b02b4418a7af1ca
Aditya8757343114 <166206608+Aditya8757343114@users.noreply.github.com>
2024-04-16 10:53:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 
 
 # VALUES REQUIRED FOR XBOTS
-API_ID = 18136872
-API_HASH = "312d861b78efcd1b02183b2ab52a83a4"
+API_ID = 26375743
+API_HASH = "49a0d82e9402201bcab5d9a5a562b6bc"
 CMD_HNDLR = getenv("CMD_HNDLR", default=".")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
